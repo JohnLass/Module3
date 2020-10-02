871d89f2734b7ec1190d5690c3bce3f4e3fb7dc0
BobbyCarangelo <robert.f.carangelo.iii.21@dartmouth.edu>
2020-10-02 13:44:44 -0400

    intial commit

---
+++ gettest1.c
@@ -0,0 +1,10 @@
+/* gettest1.c --- test getting from an empty list
+ * 
+ * 
+ * Author: Robert F. Carangelo III
+ * Created: Fri Oct  2 13:43:40 2020 (-0400)
+ * Version: 
+ * 
+ * Description: 
+ * 
+ */
